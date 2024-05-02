60610725771bf9d9fd292a4cfd5f9ecb8a759bc7
baibhavmandal <baibhavmandal1@gmail.com>
2024-05-02 20:30:51 +0530

    Created Digital JavaScript Drum Kit. Added drum kit interface with HTML, styled drum kit with CSS and JavaScipt - Audio Functionality

---
+++ Digital-JavaScipt-Drum-Kit/script.js
@@ -0,0 +1,25 @@
+const keys = document.querySelectorAll(".key");
+
+window.addEventListener("keydown", function (event) {
+  playSound(event.key);
+  console.log(event.key);
+  console.log("Hello World");
+});
+
+function playSound(key) {
+  const audio = document.querySelector(
+    `audio[data-key="${key.toUpperCase()}"]`
+  );
+  const keyElement = document.querySelector(
+    `.key[data-key="${key.toUpperCase()}"]`
+  );
+  if (!audio) return;
+
+  keyElement.classList.add("active");
+  audio.currentTime = 0;
+  audio.play();
+
+  setTimeout(() => {
+    keyElement.classList.remove("active");
+  }, 100);
+}
